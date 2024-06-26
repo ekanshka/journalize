129cdea318b483d4feef0da4c9213eceb694a96f
Ekansh <efuforofficial@gmail.com>
2024-06-26 23:03:02 +0530

    added prisma and its models

--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -1,9 +1,17 @@
 import { Hono } from "hono";
+import { signup } from "../controllers/userController";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-export const userRouter = new Hono();
+export const userRouter = new Hono<{ Bindings: { DATABASE_URL: string } }>();
 
 userRouter.post("/signup", (c) => {
-  return c.text("post user signup!");
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  return c.text(`post user signup! ${c.env.DATABASE_URL}`);
 });
 
 userRouter.post("/signin", (c) => {
