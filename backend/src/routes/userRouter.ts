fdd769642af80a40c65c1c210bb7803e9788a3ad
Ekansh <efuforofficial@gmail.com>
2024-06-27 18:51:27 +0530

    made user routes with jwt

--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -1,19 +1,68 @@
 import { Hono } from "hono";
-import { signup } from "../controllers/userController";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign, verify } from 'hono/jwt'
 
-export const userRouter = new Hono<{ Bindings: { DATABASE_URL: string } }>();
+export const userRouter = new Hono<{ Bindings: { DATABASE_URL: string, JWT_SECRET: string } }>();
 
-userRouter.post("/signup", (c) => {
+userRouter.post("/signup", async (c) => {
 
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
 
-  return c.text(`post user signup! ${c.env.DATABASE_URL}`);
+  const body = await c.req.json();
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password
+      }
+    })
+  
+    const token = await sign({userId: user.id}, c.env.JWT_SECRET);
+  
+    return c.json({token})
+
+  } catch (error) {
+    c.status(401)
+    return c.json({msg: "error while signing up"});
+  }
+
 });
 
-userRouter.post("/signin", (c) => {
-  return c.text("post user signin!");
+userRouter.post("/signin", async (c) => {
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: body.email,
+        password: body.password
+      }
+    })
+
+    if (!user) {
+      c.status(403);
+      return c.json({
+        msg: "no such user found"
+      })
+    } 
+
+    const token = await sign({userId: user.id}, c.env.JWT_SECRET)
+    return c.json({token})
+
+  } catch (error) {
+    
+    c.status(403);
+    return c.json({
+      msg: "email or password wrong"
+    })
+  }
 });
