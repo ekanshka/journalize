09968752071d46fbab4228502d278e388abc9e5d
Ekansh <efuforofficial@gmail.com>
2024-08-18 01:31:56 +0530

    added vercel.json and 404 redirect

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,5 +1,18 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const NotFound = () => {
+  
+  const navigate = useNavigate();
+
+  const redirect = () => {
+    navigate("/blogs")
+  }
+
+  useEffect(() => {
+    redirect();
+  }, [])
+
   return (
     <div className="text-red-600 bold text-5xl w-full flex justify-center mt-16">
         404 Not Found
