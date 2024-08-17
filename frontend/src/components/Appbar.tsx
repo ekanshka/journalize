a8bed4b46ff4173659f28408844a671e029b007e
Ekansh <efuforofficial@gmail.com>
2024-08-18 01:57:52 +0530

    fixed some ui

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -17,7 +17,7 @@ export const Appbar = () => {
         <div className="">
           <Link to={"/create-blog"}><button type="button" className="mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 ">New</button></Link>
           <Avatar name="Ekansh"/>
-          <button type="button" onClick={handleLogout} className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Logout</button>
+          <button type="button" onClick={handleLogout} className="ml-4 focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Logout</button>
         </div>
     </div>
   )
